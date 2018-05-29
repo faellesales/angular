bb9aec3f2dab6e7d749d7de7f4d73f794591d00a
Rafaelle Sales <faellesales@gmail.com>
2018-05-29 17:12:55 -0300

    implentacao dos dados da compra

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,14 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { ShoppingCartService } from './shopping-cart.service';
 
 @Component({
   selector: 'mt-shopping-cart',
   templateUrl: './shopping-cart.component.html'
 })
+
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shoppingCartService: ShoppingCartService) { }
 
   ngOnInit() {
   }
 
+  items(): any[]{
+    return this.shoppingCartService.items;
+  }
+
+  total(): number {
+    return this.shoppingCartService.total();
+  }
+
+  addItem(item: any){
+    this.shoppingCartService.addItem(item);
+  }
+
+  removeItem(item: any){
+    this.shoppingCartService.removeItem(item);
+  }
+
+  clear(){
+    this.shoppingCartService.clear();
+  }
 }
